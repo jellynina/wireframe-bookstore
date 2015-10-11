c18a546c63c7d106eec0a9feaad1c39da5a79190
jellynina <jellynina@gmail.com>
2015-10-12 02:02:00 +0800

    initial commitment.

---
+++ javascripts/script.js
@@ -0,0 +1,13 @@
+
+// banner Carousel
+var owl = $('#mainBanner');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    // autoplay:true,
+    // autoplayHoverPause:false,
+    // autoplayTimeout: 3000,
+    rtl: false,
+    nav: true,
+    navText: ["<span class='glyphicon glyphicon-chevron-left'></span>","<span class='glyphicon glyphicon-chevron-right'></span>"]
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+# on-line bookstore template
+
+## plugins
+
+* bootstrap
+* jQuery
+* owlCarousel2
+
+> todo: do it with bower
